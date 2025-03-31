bd04d9cd1d58ea7fab605fe125c0e334bedcfd4d
olili27 <timmolili27@gmail.com>
2025-03-31 18:07:06 +0300

    use strictEqual instead of equal

--- test/test.js
+++ test/test.js
@@ -31,31 +31,31 @@ import { multiply } from "../multiply.js";
 describe("Multiply Function", function () {
   describe("Squares of Positive Numbers", function () {
     it("multiply 1 by 1 and return 1", function () {
-      assert.equal(multiply(1, 1), 1);
+      assert.strictEqual(multiply(1, 1), 1);
     });
 
     it("multiply 2 by 2 and return 4", function () {
-      assert.equal(multiply(2, 2), 4);
+      assert.strictEqual(multiply(2, 2), 4);
     });
 
     it("multiply 3 by 3 and return 9", function () {
-      assert.equal(multiply(3, 3), 9);
+      assert.strictEqual(multiply(3, 3), 9);
     });
 
     it("multiply 4 by 4 and return 16", function () {
-      assert.equal(multiply(4, 4), 16);
+      assert.strictEqual(multiply(4, 4), 16);
     });
 
     it("multiply 5 by 5 and return 25", function () {
-      assert.equal(multiply(5, 5), 25);
+      assert.strictEqual(multiply(5, 5), 25);
     });
 
     it("multiply 10 by 10 and return 100", function () {
-      assert.equal(multiply(10, 10), 100);
+      assert.strictEqual(multiply(10, 10), 100);
     });
 
     it("multiply 25 by 25 and return 625", function () {
-      assert.equal(multiply(25, 25), 625);
+      assert.strictEqual(multiply(25, 25), 625);
     });
   });
 
@@ -63,11 +63,11 @@ describe("Multiply Function", function () {
 
   describe("Non-Squares: Multiplication of Positive Numbers", function () {
     it("multiply 23 by 45 and return 1035", function () {
-      assert.equal(multiply(23, 45), 1035);
+      assert.strictEqual(multiply(23, 45), 1035);
     });
 
     it("multiply 2 by 12 and return 24", function () {
-      assert.equal(multiply(2, 12), 24);
+      assert.strictEqual(multiply(2, 12), 24);
     });
   });
 
