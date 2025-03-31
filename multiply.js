d2a52a7cf51d22e90c7417ee268aeeeea66e7a9e
olili27 <timmolili27@gmail.com>
2025-03-31 16:49:01 +0300

    refactor multiply
    use a loop and addition to mimic multplication in a brute force way

--- multiply.js
+++ multiply.js
@@ -1,26 +1,28 @@
 /* eslint-disable operator-assignment */
 // eslint-disable-next-line import/prefer-default-export
 export function multiply(num1, num2) {
-  if (num1 === 1 && num2 === 1) {
-    return 1;
-  }
-
-  if (num1 === 2 && num2 === 2) {
-    return 4;
-  }
-
-  if (num1 === 3 && num2 === 3) {
-    return 9;
-  }
-
-  if (num1 === 4 && num2 === 4) {
-    return 16;
-  }
+  // if (num1 === 1 && num2 === 1) {
+  //   return 1;
+  // }
+  // if (num1 === 2 && num2 === 2) {
+  //   return 4;
+  // }
+  // if (num1 === 3 && num2 === 3) {
+  //   return 9;
+  // }
+  // if (num1 === 4 && num2 === 4) {
+  //   return 16;
+  // }
+  // let sum = 0;
+  // for (let i = 1; i <= 45; i = i + 1) {
+  //   sum = sum + 23;
+  // }
+  // return sum;
 
   let sum = 0;
-
-  for (let i = 1; i <= 45; i = i + 1) {
-    sum = sum + 23;
+  for (let i = 1; i <= num2; i = i + 1) {
+    sum = sum + num1;
   }
+
   return sum;
 }
