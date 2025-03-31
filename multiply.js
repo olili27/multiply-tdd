ce118a5e97ca2f80bd558a5dfeaf039ebe9ae75d
olili27 <timmolili27@gmail.com>
2025-03-31 11:00:38 +0300

    change the eslint config file name to from eslintrc.json to .eslintrc.json
    enforce the eslint rules and also by pass rules we didnt agree with as a team

--- multiply.js
+++ multiply.js
@@ -1,23 +1,25 @@
+/* eslint-disable operator-assignment */
+// eslint-disable-next-line import/prefer-default-export
 export function multiply(num1, num2) {
-    if (num1==1 && num2==1){
-        return 1
-    }
+  if (num1 === 1 && num2 === 1) {
+    return 1;
+  }
 
-    if (num1 == 2 && num2 == 2) {
-      return 4;
-    }
+  if (num1 === 2 && num2 === 2) {
+    return 4;
+  }
 
-    if (num1 == 3 && num2 == 3) {
-      return 9;
-    }
-    if (num1 ==4 && num2 == 4){
-        return 16;
-    }
-    
-    let sum = 0;
+  if (num1 === 3 && num2 === 3) {
+    return 9;
+  }
+  if (num1 === 4 && num2 === 4) {
+    return 16;
+  }
 
-for (let i = 1; i<= 45; i++){
+  let sum = 0;
+
+  for (let i = 1; i <= 45; i = i + 1) {
     sum = sum + 23;
-}
-    return sum;
+  }
+  return sum;
 }
--- test/test.js
+++ test/test.js
@@ -1,23 +1,28 @@
-import { assert } from "chai"
-import { multiply } from "../multiply.js";
+/* eslint-disable import/extensions */
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable no-undef */
+import { assert } from 'chai';
+import { multiply } from '../multiply.js';
 
-describe("Multiply Function", function() {
-    it("multiply 1 by 1 and return 1", function() {
-        assert.equal(multiply(1, 1), 1);
-    });
+describe('Multiply Function', function () {
+  it('multiply 1 by 1 and return 1', function () {
+    assert.equal(multiply(1, 1), 1);
+  });
 
-    it ("multiply 2 by 2 and return 4", function(){
-        assert.equal(multiply(2, 2), 4)
-    })
+  it('multiply 2 by 2 and return 4', function () {
+    assert.equal(multiply(2, 2), 4);
+  });
 
-    it("multiply 3 by 3 and return 9", function () {
-      assert.equal(multiply(3, 3), 9);
-    });
+  it('multiply 3 by 3 and return 9', function () {
+    assert.equal(multiply(3, 3), 9);
+  });
 
-    it("multiply 4 by 4 and return 16", function () {
-      assert.equal(multiply(4, 4), 16);
-    });
-    it("multiply 23 by 45 and return 1035", function () {
-        assert.equal(multiply(23, 45), 23*45);
-      });
+  it('multiply 4 by 4 and return 16', function () {
+    assert.equal(multiply(4, 4), 16);
+  });
+
+  it('multiply 23 by 45 and return 1035', function () {
+    assert.equal(multiply(23, 45), 23 * 45);
+  });
 });
