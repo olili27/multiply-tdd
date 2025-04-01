eae1dd58d171a9245c8b57a91d4f7919c3b4c6e5
olili27 <timmolili27@gmail.com>
2025-04-01 10:51:10 +0300

    add testcases for squares for negative numbers

--- test/test.js
+++ test/test.js
@@ -59,7 +59,35 @@ describe("Multiply Function", function () {
     });
   });
 
-  describe("Squares of Negative Numbers", function () {});
+  describe("Squares of Negative Numbers", function () {
+    it("multiply -1 by -1 and return 1", function () {
+      assert.strictEqual(multiply(1, 1), 1);
+    });
+
+    it("multiply -2 by -2 and return 4", function () {
+      assert.strictEqual(multiply(2, 2), 4);
+    });
+
+    it("multiply -3 by -3 and return 9", function () {
+      assert.strictEqual(multiply(3, 3), 9);
+    });
+
+    it("multiply -4 by -4 and return 16", function () {
+      assert.strictEqual(multiply(4, 4), 16);
+    });
+
+    it("multiply -5 by -5 and return 25", function () {
+      assert.strictEqual(multiply(5, 5), 25);
+    });
+
+    it("multiply -10 by -10 and return 100", function () {
+      assert.strictEqual(multiply(10, 10), 100);
+    });
+
+    it("multiply -25 by -25 and return 625", function () {
+      assert.strictEqual(multiply(25, 25), 625);
+    });
+  });
 
   describe("Non-Squares: Multiplication of Positive Numbers", function () {
     it("multiply 23 by 45 and return 1035", function () {
